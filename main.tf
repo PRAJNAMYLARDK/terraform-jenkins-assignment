0c71f57b6034ce24363d6ef5f4e3e62c25976e33
PRAJNAMYLARDK <106369972+PRAJNAMYLARDK@users.noreply.github.com>
2026-08-26 12:22:26 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  availability_zone   = var.availability_zone
+  vpc_name            = var.vpc_name
+}
